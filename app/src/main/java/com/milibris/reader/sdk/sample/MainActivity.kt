566a9fc8a905a12c9275b4f47ff0445820cdb69b
hadidi <mouhamedhadi.hassine@gmail.com>
2022-08-31 11:39:58 +0100

    Initial Commit With sample project

---
+++ app/src/main/java/com/milibris/reader/sdk/sample/MainActivity.kt
@@ -0,0 +1,193 @@
+package com.milibris.reader.sdk.sample
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.view.updateLayoutParams
+import co.cafeyn.onereader.data.session.ReaderSettings
+import co.cafeyn.onereader.feature.OneReaderActivity
+import com.milibris.reader.XmlPdfReaderDataSource
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+/**
+ * Main activity unpacking and reading a content with PDF reader.
+ *
+ *
+ * Milibris
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var productRepo: XmlPdfReaderDataSource
+    private val pdfName = "milibris_sample"
+    private var coverRatio: Float = 0f
+    private var targetPage: Int = 1
+    private val readerSettings = ReaderSettings().apply {
+        isFaceCropEnabled = true
+        debugBoxes = true
+        targetPageNumber = targetPage
+        textToSpeechEnabled = true
+        shareEnabled = true
+        enableSummaryImages = true
+        logo = R.drawable.milibris
+    }
+    private lateinit var coverImageURL: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        prepareProductRepo()
+        prepareCoverToTestSharedElementTransition()
+    }
+
+    private fun prepareCoverToTestSharedElementTransition() {
+        productRepo.getResourceThumbnailUri(
+            1,
+            {
+                coverImageURL = it.toString()
+                findViewById<ImageView>(R.id.coverImage).apply {
+                    val firstPage = productRepo.materialParser.mPages[0]
+                    coverRatio = firstPage.width.toFloat() / firstPage.height.toFloat()
+                    updateLayoutParams<ConstraintLayout.LayoutParams> {
+                        dimensionRatio = "${firstPage.width.toFloat() / firstPage.height.toFloat()}"
+                    }
+                    setImageURI(it)
+                    setOnClickListener {
+                        try {
+                            openReader()
+                        } catch (exception: Exception) {
+                            Log.e(
+                                "Failed to Open Reader",
+                                "Error while opening reader cause : ${exception.message}"
+                            )
+                        }
+                    }
+                }
+            },
+            {
+            }
+        )
+        findViewById<View>(R.id.buttonOpen).setOnClickListener {
+            try {
+                openReader()
+            } catch (exception: Exception) {
+                Log.e(
+                    "Failed to Open Reader",
+                    "Error while opening reader cause : ${exception.message}"
+                )
+            }
+        }
+    }
+
+    private fun prepareProductRepo() {
+        // Initialize the reader to open the contents
+        productRepo = XmlPdfReaderDataSource(readerSettings)
+        productRepo.init(applicationContext, contentPath)
+    }
+
+    private fun openReader() {
+
+        // For fragment
+        /**
+         val fragment = OneReaderFragment.newInstance(productRepo,readerSettings,readerListener)
+         supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+         .commitNow()
+         **/
+        // for activity
+        val activityOptionsCompat: ActivityOptionsCompat =
+            ActivityOptionsCompat.makeSceneTransitionAnimation(
+                this,
+                findViewById<ImageView>(R.id.coverImage),
+                OneReaderActivity.SHARED_ELEMENT_TRANSITION_NAME
+            )
+        startActivity(
+            OneReaderActivity.newIntent(
+                this,
+                readerSettings,
+                productRepo,
+                ORListener(dataSource = productRepo, "issueMid", this),
+                coverImageURL,
+                coverRatio
+            ),
+            activityOptionsCompat.toBundle()
+
+        )
+    }
+
+    // Unpack archive
+    // Unpack content if not done already
+    private val contentPath: String
+        get() {
+            val contentFile = File(getExternalFilesDir(null), pdfName)
+
+            // Unpack content if not done already
+            if (!contentFile.isDirectory) {
+
+                // Copy archive file to writable directory
+                copySampleArchiveToDisk()
+
+                // Unpack archive
+                unpackArchive()
+            }
+            if (contentFile.isDirectory) {
+                Log.i(TAG, "Content available at " + contentFile.absolutePath)
+            }
+            return contentFile.absolutePath
+        }
+
+    private fun copySampleArchiveToDisk() {
+        val assetManager = assets
+        val filename = "$pdfName.complete"
+        var `in`: InputStream? = null
+        var out: OutputStream? = null
+        try {
+            `in` = assetManager.open(filename)
+            val outFile = File(getExternalFilesDir(null), filename)
+            out = FileOutputStream(outFile)
+            val buffer = ByteArray(1024)
+            var read: Int
+            while (`in`.read(buffer).also { read = it } != -1) {
+                out.write(buffer, 0, read)
+            }
+        } catch (e: IOException) {
+            Log.e(TAG, "Failed to copy asset file: $filename", e)
+        } finally {
+            if (`in` != null) {
+                try {
+                    `in`.close()
+                } catch (e: IOException) {
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close()
+                } catch (e: IOException) {
+                }
+            }
+        }
+    }
+
+    private fun unpackArchive() {
+        val foundationContext: com.milibris.foundation.FoundationContext =
+            com.milibris.foundation.Foundation.createContext(applicationContext)
+        try {
+            val archive: com.milibris.foundation.CompleteArchive =
+                com.milibris.foundation.CompleteArchive(
+                    foundationContext,
+                    assets.openFd("$pdfName.complete").createInputStream()
+                )
+            archive.unpackTo(File(getExternalFilesDir(null), pdfName))
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+}
---
+++ app/src/main/java/com/milibris/reader/sdk/sample/ORListener.kt
@@ -0,0 +1,104 @@
+package com.milibris.reader.sdk.sample
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.core.app.ShareCompat
+import androidx.core.text.HtmlCompat
+import co.cafeyn.onereader.data.article.IArticle
+import co.cafeyn.onereader.data.article.TextToPlayState
+import co.cafeyn.onereader.data.product.Box
+import co.cafeyn.onereader.data.session.ReaderListener
+import co.cafeyn.onereader.feature.OneReaderActivity
+import com.milibris.reader.XmlPdfReaderDataSource
+import i0.b
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.nio.charset.Charset
+
+class ORListener(
+    val dataSource: XmlPdfReaderDataSource,
+    private val issueMid: String,
+    private val context: Context
+) : ReaderListener {
+
+    companion object {
+        val TAG: String = ORListener::class.java.simpleName
+    }
+    private var targetPage: Int = 0
+
+    override fun onCloseButtonClickListener() {
+        Log.e(TAG, "onCloseButtonClickListener Called")
+    }
+
+    override fun onIssueRead() {
+        Log.e(TAG, "onIssueRead")
+    }
+
+    override fun onIssuePageRead(pageNumber: Int, isCalledFromArticles: Boolean) {
+        targetPage = pageNumber
+        Log.e(TAG, "onIssuePageRead at page : $pageNumber")
+    }
+
+    override fun onSummaryOpened(isOpenedFromArticles: Boolean) {
+        Log.e(TAG, "onSummaryOpened isOpenedFromArticles:: $isOpenedFromArticles")
+    }
+
+    override fun onShareClicked(article: IArticle) {
+        val milibrisArticle = dataSource.getMilibrisArticle(article)
+        val articleUrl =
+            "${dataSource.readerSettings.shareUrl}/share/article/$issueMid/${milibrisArticle?.mid}"
+        val intentBuilder = ShareCompat.IntentBuilder(context)
+            .setType("text/plain")
+            .setText(articleUrl)
+        intentBuilder.startChooser()
+    }
+
+    override fun getTextToPlay(article: IArticle): String {
+
+        return article.getTextToPlayFileUrl()?.let { fileUrl ->
+            val fileUri = Uri.parse(fileUrl)
+            // parsing using an uri in order to have an url starting with /storage instead of file://
+            fileUri.path?.let {
+                val content = FileUtils.readFileToString(File(it), Charset.defaultCharset())
+                val startHead = content.indexOf("<head>")
+                val endHead = content.indexOf("</head>") + 7
+                val contentNoHead = String.format(
+                    "%s%s",
+                    content.substring(0, startHead),
+                    content.substring(endHead)
+                )
+                HtmlCompat.fromHtml(contentNoHead, HtmlCompat.FROM_HTML_MODE_LEGACY)
+                    .toString().replace("[\\n\\r\\t]+", "")
+            } ?: ""
+        } ?: ""
+    }
+
+    override fun onArticlesOpened(article: IArticle) {
+        Log.e(TAG, "ORListener :: article title  ${article.title}")
+    }
+
+    override fun onArticleChange(article: IArticle) {
+        Log.e(TAG, "onArticleChange :: article title  ${article.title}")
+    }
+
+    override fun onArticleBookMarkClicked(p0: IArticle, p1: Boolean, p2: b<Boolean>) {
+    }
+
+    override fun onMiniSummaryOpened() {
+        Log.e(TAG, "onMiniSummaryOpened")
+    }
+
+    override fun onTextToPlayStateChanged(article: IArticle, state: TextToPlayState) {
+        Log.e(TAG, "onTextToPlayListener: article: ${article.id}, state : $state")
+    }
+
+    override fun onEnrichmentClicked(box: Box) {
+        Log.e("ORListener", "onEnrichmentClicked")
+    }
+
+    override fun shouldOpenArticle(p0: OneReaderActivity, p1: IArticle): Boolean {
+        Log.e("ORListener", "shouldOpenArticle")
+        return true
+    }
+}
